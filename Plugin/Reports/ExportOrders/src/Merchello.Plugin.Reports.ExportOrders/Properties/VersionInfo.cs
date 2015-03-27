0edb20128a720bf17dfe4104b3baf98f6c4a2ba8
Mark Bowser <mark@proworks.com>
2015-03-27 12:03:41 -0700

    upgraded export orders plugin to merchello 1.8.0

--- Plugin/Reports/ExportOrders/src/Merchello.Plugin.Reports.ExportOrders/Properties/VersionInfo.cs
+++ Plugin/Reports/ExportOrders/src/Merchello.Plugin.Reports.ExportOrders/Properties/VersionInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.145.49668")]
+[assembly: AssemblyFileVersion("1.0.146.32496")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
 
 
